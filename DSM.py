76ebce5f3ee9613a2f5c346cde7716b3502bbdcd
小澤秀明 <>
2020-05-29 11:00:50 +0900

    ADD: different matrix

--- DSM.py
+++ DSM.py
@@ -10,6 +10,8 @@ import networkx as nx
 from scipy import stats as st
 import seaborn as sns
 
+import math
+
 
 
 def CreateDSM(path='./excel/Machinery_Label_Matrix.xlsx',
@@ -45,13 +47,13 @@ def CreateDSM(path='./excel/Machinery_Label_Matrix.xlsx',
                 # chi-square test
                 chi2, p, dof, ef = st.chi2_contingency(cross, correction=False)
 
-                if chi2 > criterion:
+                if p < 1.0e-5:
                     DSM[i][j] = 1
                     DSM[j][i] = 1
 
+                    # prepare to make npy file
                     namei = subsections[i]
                     namej = subsections[j]
-                    # weight = float(-np.log(p)/10)
                     weight_Array.append((namei, namej))
 
 
@@ -85,15 +87,6 @@ def CreateDSM(path='./excel/Machinery_Label_Matrix.xlsx',
 def CreateNetwork(savename='Machinery'):
 
     print('Create Network')
-    # df = pd.read_excel(path, sheet_name=0, header=0, index_col=0)
-    # subsections = [str(x) for x in df.index]
-    # n = len(subsections)
-    #
-    # edge_list = []
-    # for i in range(n):
-    #     for j in range(i+1, n):
-    #         if df.iloc[i][j] == 1:
-    #             edge_list.append([subsections[i], subsections[j]])
     weighted_edges = np.load('./excel/DSM/' + savename + '_weight.npy',
                              allow_pickle=True)
 
@@ -151,6 +144,83 @@ def connect_xlsx(path_g='./excel/General_Label_Matrix.xlsx',
     print('{0} Writing Succeed!'.format(save_path))
 
 
+def difference_dsm(path='./excel/Machinery_Label_Matrix.xlsx',
+                   savename='MachineryD'):
+
+    print('Create DSM of', savename)
+
+    df = pd.read_excel(path, sheet_name=0, header=0, index_col=0)
+    subsections = [str(x) for x in df.columns]
+    num_ships = len(df) - 1
+    num_subsec = len(subsections)
+
+    DSM = np.zeros((num_subsec, num_subsec), dtype='float64')
+    tf_matrix = []
+    count = 0
+
+    for i in range(num_ships):
+        for j in range(i+1, num_ships):
+            label0 = df.iloc[i]
+            label1 = df.iloc[j]
+
+            tf_series = (label0 != label1)
+            tf_matrix.append(tf_series)
+            count += 1
+
+    tf_matrix = np.array(tf_matrix)
+    count_array = np.count_nonzero(tf_matrix, axis=0) / count
+    # print(count_array)
+
+    for ship in range(num_ships):
+        for i in range(num_subsec):
+            for j in range(i+1, num_subsec):
+                if tf_matrix[ship][i] and tf_matrix[ship][j]:
+                    DSM[i][j] += 1  # (1 - count_array[i])*(1 - count_array[j])
+                    DSM[j][i] += 1  # (1 - count_array[i])*(1 - count_array[j])
+
+    df = pd.DataFrame(DSM, columns=subsections, index=subsections)
+
+    dir = './excel/DSM/'
+    os.makedirs(dir, exist_ok=True)
+
+    sheet_name = 'DSM'
+    df.to_excel(dir + savename + '_DSM.xlsx', sheet_name)
+    print(dir + savename + '_DSM.xlsx Writing Succeed!')
+
+
+def CreateWeightedNetwork(savename='MachineryD'):
+
+    print('Create Network')
+    path = './excel/DSM/' + savename + '_DSM.xlsx'
+    df = pd.read_excel(path, sheet_name=0, header=0, index_col=0)
+    subsections = [str(x) for x in df.index]
+    n = len(subsections)
+    edge_list = []
+    for i in range(n):
+        for j in range(i+1, n):
+            value = df.iloc[i][j]
+            if value >= 60:
+                edge_list.append([subsections[i], subsections[j], value])
+
+    G = nx.Graph()
+    G.add_weighted_edges_from(edge_list)
+
+    plt.figure(figsize=(10, 10))
+    pos = nx.spring_layout(G)
+    width = [(d["weight"]-50) * 0.08 for (u, v, d) in G.edges(data=True)]
+    nx.draw_networkx_nodes(G, pos, node_color='b', alpha=0.4)  # node_size=size
+    nx.draw_networkx_edges(G, pos, edge_color='r', alpha=0.2, width=width)
+    nx.draw_networkx_labels(G, pos, fontsize=6)
+
+    plt.axis('off')
+
+    save_dir = './fig/NETWORK/'
+    os.makedirs(save_dir, exist_ok=True)
+    plt.savefig(save_dir + savename + '_network.png')
+
+    print(save_dir + savename + '_network.png Writing Succeed!')
+
+
 if __name__ == '__main__':
     # CreateDSM()
     # CreateNetwork()
@@ -159,5 +229,14 @@ if __name__ == '__main__':
     # CreateDSM(path='./excel/Generanl&Machinery_Label_Matrix.xlsx', savename='Connect')
     # CreateNetwork(savename='Connect')
 
-    CreateDSM(path='./excel/General_Label_Matrix.xlsx', savename='General')
-    CreateNetwork(savename='General')
+    # CreateDSM(path='./excel/General_Label_Matrix.xlsx', savename='General')
+    # CreateNetwork(savename='General')
+
+    # difference_dsm()
+    # CreateWeightedNetwork()
+
+    # difference_dsm(path='./excel/Generanl&Machinery_Label_Matrix.xlsx', savename='ConnectD')
+    # CreateNetwork(savename='ConnectD')
+
+    # difference_dsm(path='./excel/General_Label_Matrix.xlsx', savename='GeneralD')
+    CreateWeightedNetwork(savename='GeneralD')
